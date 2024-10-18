ce191b9c45f8c9dff74a6b468bb318c402737f98
EmilioCorDev <emilio.cordova.itse@gmail.com>
2024-10-17 18:21:36 -0600

    Subiendo Repositorio Emilio Córdova

---
+++ scripts/insertarFooter.js
@@ -0,0 +1,15 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('../pages/footer.html')
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('footer--contenido').innerHTML = data;
+
+      })
+
+
+
+      .catch(error => console.error('Error loading navigation:', error));
+  });
+  
+  // fetch('../pages/footer.html')
